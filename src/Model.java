e2533b31e3ab2145664b4d4a6b5743029cc6a91b
erkkinen.b <erkkinen.b@outlook.com>
2016-06-17 14:41:59 -0400

    Adding in SSL ignore and automatic retry.

--- src/Model.java
+++ src/Model.java
@@ -2,6 +2,7 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
+import java.util.concurrent.ExecutionException;
 
 /**
  * Created by dschonholtz on 6/16/2016.
@@ -14,24 +15,34 @@ public class Model implements IModel{
     private String dburl = "jdbc:mysql://localhost:3306/finalporject";
                                         //I SPELLED IT WRONG WHILE BUILDING THE DATABASE OK???
     private Connection conn;
-    public Model() {
+
+  /**
+   * Constructor for the Model.
+   */
+  public Model() {
         data = new ArrayList<>();
     }
 
-    public void connect(String username, String password) {
+    public void connect(String username, String password) throws Exception {
         this.username = username;
         this.password = password;
         Properties connectionProps = new Properties();
         connectionProps.put("user", this.username);
         connectionProps.put("password", this.password);
+        boolean isConnected = false;
 
-        try {
-            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/finalporject", connectionProps);
-            System.out.println("Connected to database");
-        } catch (Exception e) {
-            System.out.println("ERROR: Could not connect to the database");
+        while (!isConnected) {
+            try {
+                conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/finalporject" +
+                        "?autoReconnect=true&useSSL=false", connectionProps);
+                isConnected = true;
+
+            } catch (Exception e) {
+                System.out.println("ERROR: Could not connect to the database");
+                isConnected = false;
+
+            }
         }
-
     }
 
     @Override
