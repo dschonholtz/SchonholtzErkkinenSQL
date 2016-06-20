280df41462719291e61462e8e9cec175d67ea508
dschonholtz <schonholtzd@gmail.com>
2016-06-20 18:40:04 -0400

    More Buttons

--- src/ConcreteView.java
+++ src/ConcreteView.java
@@ -7,41 +7,25 @@ import java.awt.event.ActionListener;
  * Created by Douglas Schonholtz on 6/17/2016.
  */
 public class ConcreteView extends JPanel {
-    Model model;
-    private boolean loggedIn;
-    private boolean buttonsDisplayed; //TODO needs to be in concreteView
+    private Model model;
+    public boolean buttonsDisplayed;
 
     public ConcreteView(Model model) {
         super();
         this.model = model;
-        this.loggedIn = false;
-        this.buttonsDisplayed = false;
     }
 
     public ConcreteView() {
         super();
         this.model = new Model();
-        this.loggedIn = false;
     }
 
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-
-        if(loggedIn) {
-            JButton showButtonsButton = new JButton("Show Buttons");//TODO needs to be in concreteView
-            showButtonsButton.setBounds(getWidth() - 100, 0, 100, 20);
-            this.add(showButtonsButton);
-            showButtonsButton.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    //buttonsDisplayed = !buttonsDisplayed;
-                }
-            });
-            //paintData(g);
-            //if(buttonsDisplayed) {
-                paintButtons(g);
-            //}
+        if(buttonsDisplayed) {
+            g.setColor(Color.DARK_GRAY);
+            g.fillRect(0, this.getHeight() / 2, this.getWidth(), this.getHeight() / 2);
         }
     }
 
@@ -52,84 +36,14 @@ public class ConcreteView extends JPanel {
         return new Dimension(width, height);
     }
 
-    public void setLoggedIn(boolean loggedIn) {
-        this.loggedIn = loggedIn;
+    public Model getModel() {
+        return model;
     }
 
-    private void paintButtons(Graphics g) {//TODO needs to be in concreteView
-        g.setColor(Color.DARK_GRAY);
-        g.fillRect(0,getHeight()*4/5,getWidth(),getHeight()); //rectangle that all buttons will be in
-        int buttonx = 50;
-        int buttony = getHeight()*4/5;
-        int buttonWidth = 150;
-        int buttonHeight = 20;
-        addBlock(g, buttonx,buttony, buttonWidth,buttonHeight);
-        buttonx += buttonWidth + 20;
-        addBed(g, buttonx,buttony, buttonWidth,buttonHeight);
-
+    public void setModel(Model model) {
+        this.model = model;
     }
-
-    private void addBlock(Graphics g, int buttonx, int buttony, int buttonWidth, int buttonHeight) {//TODO needs to be in concreteView
-        JButton addBlockButton = new JButton("Add/Update Block");
-        addBlockButton.setBounds(buttonx, buttony, buttonWidth, buttonHeight);
-        this.add(addBlockButton);
-        addBlockButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-            }
-        });
-
-        JLabel blockIDLabel = new JLabel("Block ID");
-        blockIDLabel.setForeground(Color.white);
-        blockIDLabel.setBounds(buttonx, buttony + buttonHeight, buttonWidth, buttonHeight);
-        this.add(blockIDLabel);
-
-        JTextField blockID = new JTextField(6);
-        blockID.setBounds(buttonx, buttony + buttonHeight * 2, buttonWidth, buttonHeight);
-        this.add(blockID);
-
-        JLabel blockNoteLabel = new JLabel("Block Notes");
-        blockNoteLabel.setForeground(Color.white);
-        blockNoteLabel.setBounds(buttonx, buttony + buttonHeight * 3, buttonWidth, buttonHeight);
-        this.add(blockNoteLabel);
-
-        JTextField notes = new JTextField(6);
-        notes.setBounds(buttonx, buttony + buttonHeight * 4, buttonWidth, buttonHeight);
-        this.add(notes);
-    }
-
-    private void addBed(Graphics g, int buttonx, int buttony, int buttonWidth, int buttonHeight) {
-        JButton addBedButton = new JButton("Add/Update Bed");
-        addBedButton.setBounds(buttonx, buttony, buttonWidth, buttonHeight);
-        this.add(addBedButton);
-        addBedButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-            }
-        });
-
-        JLabel blockIDLabel = new JLabel("Block ID");
-        blockIDLabel.setForeground(Color.white);
-        blockIDLabel.setBounds(buttonx, buttony + buttonHeight, buttonWidth, buttonHeight);
-        this.add(blockIDLabel);
-
-        JTextField blockID = new JTextField(6);
-        blockID.setBounds(buttonx, buttony + buttonHeight * 2, buttonWidth, buttonHeight);
-        this.add(blockID);
-
-        JLabel blockNoteLabel = new JLabel("Bed Notes");
-        blockNoteLabel.setForeground(Color.white);
-        blockNoteLabel.setBounds(buttonx, buttony + buttonHeight * 3, buttonWidth, buttonHeight);
-        this.add(blockNoteLabel);
-
-        JTextField notes = new JTextField(6);
-        notes.setBounds(buttonx, buttony + buttonHeight * 4, buttonWidth, buttonHeight);
-        this.add(notes);
-    }
-
-    /**
+/**
      * harvest button
      * valid bed
      *  - button
--- src/View.java
+++ src/View.java
@@ -21,6 +21,7 @@ import java.awt.event.ActionListener;
 public class View extends JFrame {
     private Model model;
     final private ConcreteView concreteView;
+    private boolean buttonsDisplayed;
 
 
     public View() {
@@ -28,6 +29,7 @@ public class View extends JFrame {
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         this.getContentPane().add(concreteView);
         this.pack();
+        buttonsDisplayed = false;
     }
 
     public void run() {
@@ -64,15 +66,204 @@ public class View extends JFrame {
                     concreteView.remove(passwordText);
                     concreteView.remove(loginButton);
                     concreteView.remove(headerLabel);
-                    concreteView.setLoggedIn(true);
                     concreteView.revalidate();
                     concreteView.repaint();
+                    begin();
                 } catch (Exception exception) {
                     statusLabel.setText("Invalid password or username please try again");
                 }
             }
         });
         concreteView.add(statusLabel);
+
+
+        //paintData(g);
+
         this.setVisible(true);
     }
+
+    private void begin() {
+        concreteView.setLayout(null);
+        JButton showButtonsButton = new JButton("Show Buttons");
+        showButtonsButton.setBounds(getWidth() - 150, 0, 150, 20);
+        concreteView.add(showButtonsButton);
+        showButtonsButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                buttonsDisplayed = !buttonsDisplayed;
+                concreteView.buttonsDisplayed = !concreteView.buttonsDisplayed;
+                showButtons();
+            }
+        });
+    }
+
+    private void showButtons() {
+        if(buttonsDisplayed) {
+
+            int buttonx = 50;
+            int buttony = concreteView.getHeight() / 2;
+            int buttonWidth = 150;
+            int buttonHeight = 20;
+            insertOrUpdateBlock(buttonx, buttony, buttonWidth, buttonHeight);
+
+            buttonx += buttonWidth + 20;
+            insertOrUpdateBed(buttonx, buttony, buttonWidth, buttonHeight);
+
+            buttonx += buttonWidth + 20;
+            inserOrUpdateCrop(buttonx, buttony, buttonWidth, buttonHeight);
+
+
+            revalidate();
+            repaint();
+        }
+        else {
+            concreteView.removeAll();
+            begin();
+            revalidate();
+            repaint();
+        }
+
+    }
+
+    private void insertOrUpdateBlock(int buttonx, int buttony, int buttonWidth, int buttonHeight) {
+        JButton addBlockButton = new JButton("Add/Update Block");
+        addBlockButton.setBounds(buttonx, buttony, buttonWidth, buttonHeight);
+        concreteView.add(addBlockButton);
+        addBlockButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        JLabel blockIDLabel = new JLabel("Block ID");
+        blockIDLabel.setForeground(Color.white);
+        blockIDLabel.setBounds(buttonx, buttony + buttonHeight, buttonWidth, buttonHeight);
+        concreteView.add(blockIDLabel);
+
+        JTextField blockID = new JTextField(6);
+        blockID.setBounds(buttonx, buttony + buttonHeight * 2, buttonWidth, buttonHeight);
+        concreteView.add(blockID);
+
+        JLabel blockNoteLabel = new JLabel("Block Notes");
+        blockNoteLabel.setForeground(Color.white);
+        blockNoteLabel.setBounds(buttonx, buttony + buttonHeight * 3, buttonWidth, buttonHeight);
+        concreteView.add(blockNoteLabel);
+
+        JTextField notes = new JTextField(6);
+        notes.setBounds(buttonx, buttony + buttonHeight * 4, buttonWidth, buttonHeight);
+        concreteView.add(notes);
+    }
+
+    private void insertOrUpdateBed(int buttonx, int buttony, int buttonWidth, int buttonHeight) {
+        JButton addBedButton = new JButton("Add/Update Bed");
+        addBedButton.setBounds(buttonx, buttony, buttonWidth, buttonHeight);
+        concreteView.add(addBedButton);
+        addBedButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        JLabel blockIDLabel = new JLabel("Block ID");
+        blockIDLabel.setForeground(Color.white);
+        blockIDLabel.setBounds(buttonx, buttony + buttonHeight, buttonWidth, buttonHeight);
+        concreteView.add(blockIDLabel);
+
+        JTextField blockID = new JTextField(6);
+        blockID.setBounds(buttonx, buttony + buttonHeight * 2, buttonWidth, buttonHeight);
+        concreteView.add(blockID);
+
+        JLabel bedIDLabel = new JLabel("Bed ID");
+        bedIDLabel.setForeground(Color.white);
+        bedIDLabel.setBounds(buttonx, buttony + buttonHeight * 3, buttonWidth, buttonHeight);
+        concreteView.add( bedIDLabel);
+
+        JTextField bedID = new JTextField(6);
+        bedID.setBounds(buttonx, buttony + buttonHeight * 4, buttonWidth, buttonHeight);
+        concreteView.add(bedID);
+
+        JLabel blockNoteLabel = new JLabel("Bed Notes");
+        blockNoteLabel.setForeground(Color.white);
+        blockNoteLabel.setBounds(buttonx, buttony + buttonHeight * 5, buttonWidth, buttonHeight);
+        concreteView.add(blockNoteLabel);
+
+        JTextField notes = new JTextField(6);
+        notes.setBounds(buttonx, buttony + buttonHeight * 6, buttonWidth, buttonHeight);
+        concreteView.add(notes);
+    }
+
+    private void inserOrUpdateCrop(int buttonx, int buttony, int buttonWidth, int buttonHeight) {
+        JLabel cropNameLabel = new JLabel("Crop Name");
+        cropNameLabel.setForeground(Color.white);
+        cropNameLabel.setBounds(buttonx, buttony + buttonHeight, buttonWidth, buttonHeight);
+        concreteView.add(cropNameLabel);
+
+        JTextField varietyField = new JTextField(6);
+        varietyField.setBounds(buttonx, buttony + buttonHeight * 2, buttonWidth, buttonHeight);
+        concreteView.add(varietyField);
+
+
+        JLabel seedSourceLabel = new JLabel("Seed Source");
+        seedSourceLabel.setForeground(Color.white);
+        seedSourceLabel.setBounds(buttonx, buttony + buttonHeight * 3, buttonWidth, buttonHeight);
+        concreteView.add(seedSourceLabel);
+
+        JTextField seedSource = new JTextField(6);
+        seedSource.setBounds(buttonx, buttony + buttonHeight * 4, buttonWidth, buttonHeight);
+        concreteView.add(seedSource);
+
+
+        JLabel numSeedsLabel = new JLabel("Number of Seeds (INT)");
+        numSeedsLabel.setForeground(Color.white);
+        numSeedsLabel.setBounds(buttonx, buttony + buttonHeight * 5, buttonWidth, buttonHeight);
+        concreteView.add(numSeedsLabel);
+
+        JTextField numSeeds = new JTextField(6);
+        numSeeds.setBounds(buttonx, buttony + buttonHeight * 6, buttonWidth, buttonHeight);
+        concreteView.add(numSeeds);
+
+
+        JLabel germinationYieldProjLabel = new JLabel("Projected Germination Yield(Double)");
+        germinationYieldProjLabel.setForeground(Color.white);
+        germinationYieldProjLabel.setBounds(buttonx, buttony + buttonHeight * 7, buttonWidth, buttonHeight);
+        concreteView.add(germinationYieldProjLabel);
+
+        JTextField germinationProj = new JTextField(6);
+        germinationProj.setBounds(buttonx, buttony + buttonHeight * 8, buttonWidth, buttonHeight);
+        concreteView.add(germinationProj);
+
+
+        JLabel germinationYieldActLabel = new JLabel("Actual Germination Yield(Double)");
+        germinationYieldActLabel.setForeground(Color.white);
+        germinationYieldActLabel.setBounds(buttonx, buttony + buttonHeight * 9, buttonWidth, buttonHeight);
+        concreteView.add(germinationYieldActLabel);
+
+        JTextField germinationAct = new JTextField(6);
+        germinationAct.setBounds(buttonx, buttony + buttonHeight * 10, buttonWidth, buttonHeight);
+        concreteView.add(germinationAct);
+        // Double feet_between_plants, String part_num, Double cost, Integer qty,
+        // String packType, String notes
+
+        JLabel feetBetweenPlantsLabel = new JLabel("Feet Between Plants(Double)");
+        feetBetweenPlantsLabel.setForeground(Color.white);
+        feetBetweenPlantsLabel.setBounds(buttonx, buttony + buttonHeight * 11, buttonWidth, buttonHeight);
+        concreteView.add(feetBetweenPlantsLabel);
+
+        JTextField feetBetweenPlants = new JTextField(6);
+        feetBetweenPlants.setBounds(buttonx, buttony + buttonHeight * 12, buttonWidth, buttonHeight);
+        concreteView.add(feetBetweenPlants);
+
+        JButton insertOrUpdateCropButton = new JButton("Add/Update Crop");
+        insertOrUpdateCropButton.setBounds(buttonx, buttony, buttonWidth, buttonHeight);
+        concreteView.add(insertOrUpdateCropButton);
+        insertOrUpdateCropButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+
 }
